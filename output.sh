b87fb69aebac5ec59499cb7827dadda5897d8e64
Nithya <nithyaasworld@gmail.com>
2021-12-23 14:33:59 +0530

    Merge branch 'master' of github.com:nithyaasworld/add-contact-chip

---
+++ output.sh
@@ -0,0 +1,20 @@
+set -eo pipefail
+
+echo "--- section 1"
+echo "some lines here"
+echo "some more testing"
+echo "--- section 2"
+echo "some lines here"
+echo "some more testing"   
+echo '--- section 3'
+echo "some lines here"
+echo "some more testing"
+echo "--- section 4"
+echo "some lines here"
+echo "some more testing"   
+echo "--- section 5"
+echo "some lines here"
+echo "some more testing"
+echo "--- section 6"
+echo "some lines here"
+echo "some more testing" 
